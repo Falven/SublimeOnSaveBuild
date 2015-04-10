8c05a9d7aebf99c0f0a47dbd2dc0441c420c0a97
Falven <Falven2000@Hotmail.com>
2015-04-10 17:59:15 -0600

    Added verbose option. Added project check (make sure project supports building), added regular expression to ingore .sublime-settings files...

--- SublimeOnSaveBuild.py
+++ SublimeOnSaveBuild.py
@@ -6,19 +6,17 @@ import re
 class SublimeOnSaveBuild(sublime_plugin.EventListener):
     def on_post_save(self, view):
         global_settings = sublime.load_settings(self.__class__.__name__+'.sublime-settings')
+        view_settings = view.settings()
+        cur_window = view.window()
 
         # See if we should build. A project level build_on_save setting
         # takes precedence. To be backward compatible, we assume the global
         # build_on_save to be true if not defined.
-        should_build = view.settings().get('build_on_save', global_settings.get('build_on_save', True))
+        build_on_save = view_settings.get('build_on_save', global_settings.get('build_on_save', True))
+        filename_filter = view_settings.get('filename_filter', global_settings.get('filename_filter', '.*'))
+        verbose = view_settings.get('verbose', global_settings.get('verbose', False))
 
-        # Load filename filter. Again, a project level setting takes precedence.
-        filename_filter = view.settings().get('filename_filter', global_settings.get('filename_filter', '.*'))
-
-        if not should_build:
-            return
-
-        if not re.search(filename_filter, view.file_name()):
-            return
-
-        view.window().run_command('build')
+        # check if it is a project and supports building...
+        if build_on_save and cur_window.project_data() and re.search(filename_filter, view.file_name()):
+            print('Save detected, running build.')
+            cur_window.run_command('build')
